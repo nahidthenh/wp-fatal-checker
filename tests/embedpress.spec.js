f9c4ff4b2fa9efaf485c7a78731754271366aa70
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-05-29 10:36:03 +0600

    New fatal Condation added

--- tests/embedpress.spec.js
+++ tests/embedpress.spec.js
@@ -1,3 +1,35 @@
+// // @ts-check
+// const { test, expect } = require('@playwright/test');
+
+// const urls = [
+//   'https://embedpress.qcheck.site/',
+//   'https://embedpress.qcheck.site/gutenberg-youtube/',
+//   'https://embedpress.qcheck.site/classic-youtube/',
+//   'https://embedpress.qcheck.site/elementor-youtube/',
+//   'https://embedpress.qcheck.site/wp-admin/',
+//   'https://sp.qcheck.site/'
+
+//   // Add more URLs to this list
+// ];
+
+// test('Check for Fatal Error on EmbedPress', async ({ page }) => {
+//   for (const url of urls) {
+//     await page.goto(url);
+
+//     // Check for the fatal error message
+//     const error = await page.getByText('Fatal error:').isVisible();
+//     await page.getByText('There has been a critical error on this website.').isVisible();
+
+
+//     if (error) {
+//       console.log(`URL: ${url} - Fatal Error`);
+//     } else {
+//       console.log(`URL: ${url} - Page loaded properly`);
+//     }
+//   }
+// });
+
+
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
@@ -7,8 +39,7 @@ const urls = [
   'https://embedpress.qcheck.site/classic-youtube/',
   'https://embedpress.qcheck.site/elementor-youtube/',
   'https://embedpress.qcheck.site/wp-admin/',
-  // 'https://embedpress.qcheck.site/classic-singles/'
-
+  'https://sp.qcheck.site/'
   // Add more URLs to this list
 ];
 
@@ -17,9 +48,10 @@ test('Check for Fatal Error on EmbedPress', async ({ page }) => {
     await page.goto(url);
 
     // Check for the fatal error message
-    const error = await page.getByText('Fatal error:').isVisible();
+    const fatalError1 = await page.getByText('Fatal error:').isVisible().catch(() => false);
+    const fatalError2 = await page.getByText('There has been a critical error on this website.').isVisible().catch(() => false);
 
-    if (error) {
+    if (fatalError1 || fatalError2) {
       console.log(`URL: ${url} - Fatal Error`);
     } else {
       console.log(`URL: ${url} - Page loaded properly`);
