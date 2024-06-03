7acbb57f0eb7ae16e711743ac57d9c8d1ccbe100
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-06-03 15:19:02 +0600

    Regular Expression

--- tests/regexp-embedpress.spec.js
+++ tests/regexp-embedpress.spec.js
@@ -62,11 +62,12 @@ const urls = [
 test('Check for Fatal Error on EmbedPress', async ({ page }) => {
   // Disables the timeout
   test.setTimeout(0);
+  // Regular expression to match PHP error messages
+  const REGEXP_PHP_ERROR = /(<b>)?(Fatal error|Recoverable fatal error|Warning|Parse error|Notice|Strict Standards|Deprecated|Unknown error)(<\/b>)?: (.*?) in (.*?) on line (<b>)?\d+(<\/b>)?/;
 
   for (const url of urls) {
     await page.goto(url);
-
-    const REGEXP_PHP_ERROR = /(<b>)?(Fatal error|Recoverable fatal error|Warning|Parse error|Notice|Strict Standards|Deprecated|Unknown error)(<\/b>)?: (.*?) in (.*?) on line (<b>)?\d+(<\/b>)?/;
+    // Check for visible fatal error messages
     const fatalError1 = await page.getByText('Fatal error:').isVisible().catch(() => false);
     const fatalError2 = await page.getByText('There has been a critical error on this website.').isVisible().catch(() => false);
 
