b2884041c57248a0ffb514af553c62dc91cfce03
nahidthenh <nhgroup18@gmail.com>
2024-06-04 20:26:39 +0600

    Code Organize

--- tests/embedpress.spec.js
+++ tests/embedpress.spec.js
@@ -1,83 +1,28 @@
 const { test, expect } = require('@playwright/test');
-const urls = [
-  'http://embedpress.qcheck.site/1-youtube-singleee',
-  'http://embedpress.qcheck.site/1-youtube-singleee/',
-  'http://embedpress.qcheck.site/classic-gumroad/',
-  'http://embedpress.qcheck.site/classic-meetup/',
-  'http://embedpress.qcheck.site/classic-nft/',
-  'http://embedpress.qcheck.site/classic-pdf/',
-  'http://embedpress.qcheck.site/classic-twitch/',
-  'http://embedpress.qcheck.site/classic-apple/',
-  'http://embedpress.qcheck.site/classic-boomplay/',
-  'http://embedpress.qcheck.site/classic-calendly/',
-  'http://embedpress.qcheck.site/classic-facebook/',
-  'http://embedpress.qcheck.site/classic-google/',
-  'http://embedpress.qcheck.site/classic-singles/',
-  'http://embedpress.qcheck.site/classic-spotify/',
-  'http://embedpress.qcheck.site/classic-youtube/',
-  'http://embedpress.qcheck.site/contact/',
-  'http://embedpress.qcheck.site/elementor-gumroad/',
-  'http://embedpress.qcheck.site/elementor-adv-test/',
-  'http://embedpress.qcheck.site/elementor-calendly/',
-  'http://embedpress.qcheck.site/elementor-instra/',
-  'http://embedpress.qcheck.site/elementor-meetup/',
-  'http://embedpress.qcheck.site/elementor-nft/',
-  'http://embedpress.qcheck.site/elementor-nrk-github-gist-acf/',
-  'http://embedpress.qcheck.site/elementor-pdf/',
-  'http://embedpress.qcheck.site/elementor-pdf-test/',
-  'http://embedpress.qcheck.site/elementor-twitch/',
-  'http://embedpress.qcheck.site/elementor-apple/',
-  'http://embedpress.qcheck.site/elementor-boomplay/',
-  'http://embedpress.qcheck.site/elementor-facebook/',
-  'http://embedpress.qcheck.site/elementor-google/',
-  'http://embedpress.qcheck.site/elementor-pdf-2/',
-  'http://embedpress.qcheck.site/elementor-singles/',
-  'http://embedpress.qcheck.site/elementor-spotify/',
-  'http://embedpress.qcheck.site/elementor-youtube/',
-  'http://embedpress.qcheck.site/guntenberg-facebook/',
-  'http://embedpress.qcheck.site/guntenberg-meetup/',
-  'http://embedpress.qcheck.site/guntenberg-pdf/',
-  'http://embedpress.qcheck.site/gutenberg-adv-feature-test/',
-  'http://embedpress.qcheck.site/gutenberg-all-single/',
-  'http://embedpress.qcheck.site/gutenberg-calendly/',
-  'http://embedpress.qcheck.site/gutenberg-calendly-2/',
-  'http://embedpress.qcheck.site/gutenberg-github-gist-nrk-acf/',
-  'http://embedpress.qcheck.site/gutenberg-nft/',
-  'http://embedpress.qcheck.site/gutenberg-opensea-nft/',
-  'http://embedpress.qcheck.site/gutenberg-pdf/',
-  'http://embedpress.qcheck.site/gutenberg-pptx/',
-  'http://embedpress.qcheck.site/gutenberg-tiktok/',
-  'http://embedpress.qcheck.site/gutenberg-twitch/',
-  'http://embedpress.qcheck.site/gutenberg-youtube-live-chat/',
-  'http://embedpress.qcheck.site/gutenberg-apple/',
-  'http://embedpress.qcheck.site/gutenberg-boomplay/',
-  'http://embedpress.qcheck.site/gutenberg-google/',
-  'http://embedpress.qcheck.site/gutenberg-gumroad/',
-  'http://embedpress.qcheck.site/gutenberg-signles/',
-  'http://embedpress.qcheck.site/gutenberg-spotify/',
-  'http://embedpress.qcheck.site/gutenberg-youtube/'
-  // Add more URLs to this list
-];
+const { urls } = require('../utils/urls/embedpress');
+
 
 test('Check for Fatal Error on EmbedPress', async ({ page }) => {
   // Disables the timeout
   test.setTimeout(0);
-
+  // Regular expression to match PHP error messages
+  const REGEXP_PHP_ERROR = /(<b>)?(Fatal error|Recoverable fatal error|Warning|Parse error|Notice|Strict Standards|Deprecated|Unknown error)(<\/b>)?: (.*?) in (.*?) on line (<b>)?\d+(<\/b>)?/;
   for (const url of urls) {
-    await test.step(`Checking URL: ${url}`, async () => {
-      await page.goto(url);
+    await page.goto(url);
+    // Check for visible fatal error messages
+    const fatalError1 = await page.getByText('Fatal error:').isVisible().catch(() => false);
+    const fatalError2 = await page.getByText('There has been a critical error on this website.').isVisible().catch(() => false);
 
-      // Check for the fatal error message
-      const fatalError1 = await page.getByText('Fatal error:').isVisible().catch(() => false);
-      const fatalError2 = await page.getByText('There has been a critical error on this website.').isVisible().catch(() => false);
+    // Get the page content
+    const pageContent = await page.content();
 
-      if (fatalError1 || fatalError2) {
-        console.log(`❌ Fatal Error - ${url}`);
-        test.info().annotations.push({ type: 'End Result', description: `URL: ${url} - ❌ Fatal Error` });
-      } else {
-        console.log(`✅ Page loaded properly - ${url}`);
-        test.info().annotations.push({ type: 'End Result', description: `URL: ${url} - ✅ Page loaded properly` });
-      }
-    });
+    // Check for the fatal error message using the regex
+    const matches = pageContent.match(REGEXP_PHP_ERROR);
+
+    if (matches || fatalError1 || fatalError2) {
+      console.log(`❌ Found Error - ${url}`);
+    } else {
+      console.log(`✅ Page loaded properly - ${url}`);
+    }
   }
 });
--- tests/essential_addons.spec.js
+++ tests/essential_addons.spec.js
@@ -1,129 +1,27 @@
 const { test, expect } = require('@playwright/test');
+const { urls } = require('../utils/urls/essential_addons');
 
-const urls = [
-
-    'https://ea.qcheck.site/',
-    'https://ea.qcheck.site/advanced-search/',
-    'https://ea.qcheck.site/simple-menu/',
-    'https://ea.qcheck.site/creative-buttons/',
-    'https://ea.qcheck.site/static-product',
-    'https://ea.qcheck.site/event-calendar',
-    'https://ea.qcheck.site/flip-box',
-    'https://ea.qcheck.site/logo-carousel',
-    'https://ea.qcheck.site/dual-color-heading',
-    'https://ea.qcheck.site/info-box',
-    'https://ea.qcheck.site/team-member-carousel',
-    'https://ea.qcheck.site/advanced-accordion',
-    'https://ea.qcheck.site/advanced-tabs',
-    'https://ea.qcheck.site/testimonial-slider',
-    'https://ea.qcheck.site/team-member',
-    'https://ea.qcheck.site/tooltip',
-    'https://ea.qcheck.site/sticky-video',
-    'https://ea.qcheck.site/testimonials',
-
-    'https://embedpress.qcheck.site/elementor-youtube/',
-
-    'https://ea.qcheck.site/protected-content',
-    'https://ea.qcheck.site/offcanvas',
-    'https://ea.qcheck.site/advanced-menu',
-    'https://ea.qcheck.site/toggle',
-    'https://ea.qcheck.site/feature-list',
-    'https://ea.qcheck.site/advanced-data-table',
-    'https://ea.qcheck.site/post-block',
-    'https://ea.qcheck.site/nft-gallery',
-    'https://ea.qcheck.site/smart-post-list',
-    'https://ea.qcheck.site/advanced-google-map',
-    'https://ea.qcheck.site/content-ticker',
-    'https://ea.qcheck.site/post-timeline',
-    'https://ea.qcheck.site/dynamic-gallery',
-    'https://ea.qcheck.site/post-grid',
-    'https://ea.qcheck.site/data-table',
-    'https://ea.qcheck.site/content-timeline',
-    'https://ea.qcheck.site/post-carousel',
-    'https://ea.qcheck.site/business-reviews',
-    'https://ea.qcheck.site/pricing-table',
-    'https://ea.qcheck.site/call-to-action',
-    'https://ea.qcheck.site/price-menu',
-    'https://ea.qcheck.site/interactive-circle',
-    'https://ea.qcheck.site/interactive-cards',
-    'https://ea.qcheck.site/interactive-promo',
-    'https://ea.qcheck.site/one-page-navigation',
-    'https://ea.qcheck.site/filterable-gallery',
-    'https://ea.qcheck.site/image-hotspots',
-    'https://ea.qcheck.site/image-scroller',
-    'https://ea.qcheck.site/image-comparison',
-    'https://ea.qcheck.site/divider',
-    'https://ea.qcheck.site/fancy-text',
-    'https://ea.qcheck.site/counter',
-    'https://ea.qcheck.site/countdown',
-    'https://ea.qcheck.site/lightbox-and-modal',
-    'https://ea.qcheck.site/flip-carousel',
-    'https://ea.qcheck.site/progress-bar',
-    'https://ea.qcheck.site/image-accordion',
-    'https://ea.qcheck.site/svg-draw',
-    'https://ea.qcheck.site/fancy-chart',
-    'https://ea.qcheck.site/typeform',
-    'https://ea.qcheck.site/contact-form-7',
-    'https://ea.qcheck.site/wpforms',
-    'https://ea.qcheck.site/ninja-forms',
-    'https://ea.qcheck.site/mailchimp',
-    'https://ea.qcheck.site/caldera-forms',
-    'https://ea.qcheck.site/login-register-form',
-    'https://ea.qcheck.site/FluentForm',
-    'https://ea.qcheck.site/weforms',
-    'https://ea.qcheck.site/formstack',
-    'https://ea.qcheck.site/gravity-forms',
-    'https://ea.qcheck.site/twitter-feed-carousel',
-    'https://ea.qcheck.site/twitter-feed',
-    'https://ea.qcheck.site/instagram-feed',
-    'https://ea.qcheck.site/facebook-feed',
-    'https://ea.qcheck.site/BetterDocs-category-box',
-    'https://ea.qcheck.site/BetterDocs-search-form',
-    'https://ea.qcheck.site/woo-thank-you',
-    'https://ea.qcheck.site/woo-cart',
-    'https://ea.qcheck.site/woo-product-carousel',
-    'https://ea.qcheck.site/woo-product-gallery',
-    'https://ea.qcheck.site/woo-checkout',
-    'https://ea.qcheck.site/woo-product-slider',
-    'https://ea.qcheck.site/woo-product-collections',
-    'https://ea.qcheck.site/woo-product-grid',
-    'https://ea.qcheck.site/woo-product-compare',
-    'https://ea.qcheck.site/woo-sross-sells',
-    'https://ea.qcheck.site/woo-account-dashboard',
-    'https://ea.qcheck.site/woo-product-list',
-    'https://ea.qcheck.site/cross-domain-copy-paste',
-    'https://ea.qcheck.site/scroll-to-top',
-    'https://ea.qcheck.site/content-protection',
-    'https://ea.qcheck.site/table-of-contents',
-    'https://ea.qcheck.site/parallax',
-    'https://ea.qcheck.site/reading-progress-bar',
-    'https://ea.qcheck.site/particles',
-    'https://ea.qcheck.site/advanced-tooltip',
-    'https://ea.qcheck.site/conditional-display',
-    'https://ea.qcheck.site/wrapper-link'
-
-    // More urls 
-
-]
-
-test('Essential Addons Fatal Error Check', async ({ page }) => {
-
+test('Essential Addons PHP Error Check', async ({ page }) => {
     // Disables the timeout
     test.setTimeout(0);
-
+    // Regular expression to match PHP error messages
+    const REGEXP_PHP_ERROR = /(<b>)?(Fatal error|Recoverable fatal error|Warning|Parse error|Notice|Strict Standards|Deprecated|Unknown error)(<\/b>)?: (.*?) in (.*?) on line (<b>)?\d+(<\/b>)?/;
     for (const url of urls) {
         await page.goto(url);
-
-        // Fatal Error Message Check 
+        // Check for visible fatal error messages
         const fatalError1 = await page.getByText('Fatal error:').isVisible().catch(() => false);
         const fatalError2 = await page.getByText('There has been a critical error on this website.').isVisible().catch(() => false);
 
-        if (fatalError1 || fatalError2) {
-            console.log(`❌ Fatal Error - ${url}`);
+        // Get the page content
+        const pageContent = await page.content();
+
+        // Check for the fatal error message using the regex
+        const matches = pageContent.match(REGEXP_PHP_ERROR);
+
+        if (matches || fatalError1 || fatalError2) {
+            console.log(`❌ Found Error - ${url}`);
         } else {
             console.log(`✅ Page loaded properly - ${url}`);
         }
-
     }
-
-})
+});
